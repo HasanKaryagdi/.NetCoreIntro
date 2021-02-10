6821ea0b78497edcc9ea4bda0bfdc81fd271c742
HasanKaryagdi <hasan.karyagdi@outlook.com>
2021-02-10 18:21:52 +0300

    OOP List

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace GenericsIntro
+{
+    class MyList<T> // T =>Type
+    {
+        T[] items;
+        //ctor =>Constructor : bir class newlendiğinde çalışsan bloga ctor denir
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items; // geçici referans ,öncekı verileri korumak için tutulur
+            items = new T[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item; 
+        }
+    }
+}
